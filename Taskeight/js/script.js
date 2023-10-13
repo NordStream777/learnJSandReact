80cd02cac3320fd30db710d86bf6f818b8960809
NordStream777 <NRDStream@gmail.com>
2023-10-13 19:45:47 +0300

    done nine Tasl + timer

--- Taskeight/js/script.js
+++ Taskeight/js/script.js
@@ -1,6 +1,7 @@
 window.addEventListener("DOMContentLoaded", function(){
-    'use scrict'
 
+    'use scrict'
+// Табы
     let container = document.querySelector(".info-header");
     let tabs = document.querySelectorAll (".info-header-tab");
     let dscr = document.querySelectorAll (".info-tabcontent")
@@ -34,4 +35,25 @@ window.addEventListener("DOMContentLoaded", function(){
         }
     })
 
+// Таймер
+// Устанавливаем дату, до которой будет идти обратный отсчет (замените эту дату на нужную)
+let countDownDate = Date.parse('2023-10-14');
+
+let x = setInterval(function() {
+
+    let now = Date.now();
+    let t = countDownDate - now;
+    let hours = Math.floor((t/(1000*3600)));
+    let minutes = Math.floor((t/1000/60)%60);
+    let seconds = Math.floor((t/1000)%60);
+
+    document.querySelector(".hours").textContent = hours < 10 ? "0" + hours : hours;
+    document.querySelector(".minutes").textContent = minutes < 10 ? "0" + minutes : minutes;
+    document.querySelector(".seconds").textContent = seconds < 10 ? "0" + seconds : seconds;
+
+    if (t < 0) {
+    clearInterval(x);
+    document.getElementById("timer").textContent = "Время истекло!";
+    }
+}, 1000);
 });
